3e0c84323839954ed7e2f45d9edfff3fd5c16ede
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-04-21 11:39:30 +0300

    added lazyloading in repoList

--- src/components/ReposList/ReposList.js
+++ src/components/ReposList/ReposList.js
@@ -1,21 +1,30 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 import useStyles from "./ReposListUITheme";
 
 const ReposList = ({ repos }) => {
   const classes = useStyles();
 
-  const tableRows = repos.data.map((repo) => {
+  const tableRows = repos.map((repo) => {
     return (
       <ListItem button key={repo.id}>
         <NavLink className={classes.link} to={repo["full_name"]}>
+          <div className={classes.imageWrapper}>
+            <LazyLoadImage
+              src={repo.owner["avatar_url"]}
+              alt="avatar"
+              effect="blur"
+              width="50"
+              height="50"
+            />
+          </div>
           <Typography component="h5" variant="h5">
             {repo.name}
           </Typography>
--- src/components/ReposList/ReposListUITheme.js
+++ src/components/ReposList/ReposListUITheme.js
@@ -1,17 +1,28 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => {
-  console.log(theme);
   return {
     link: {
       textDecoration: "none",
-      display: "block"
+      display: "flex",
+      width: "100%",
+      alignItems: "center",
     },
     card: {
       textDecoration: "none",
       "&:hover": {
         background: theme.palette.action.hover,
       },
+    },
+    imageWrapper: {
+      minWidth: 50,
+      minHeight: 50,
+      marginRight: theme.spacing(1),
+    },
+    title: {
+      overflow: "hidden",
+      textOverflow: "ellipsis",
+      whiteSpace: "nowrap",
     },
   };
 });
