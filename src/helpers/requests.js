f0bba7d9230492e63e713072ea259d2ecd4e9d16
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-04-21 18:14:46 +0300

    written cors interceptor handler

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,4 +1,4 @@
-export const withResponseTimeMeasures = (axiosInstance) => {
+export const addInterceptors = (axiosInstance, interceptors) => {
   let startTime;
   let endTime;
 
@@ -8,6 +8,7 @@ export const withResponseTimeMeasures = (axiosInstance) => {
       return config;
     },
     (error) => {
+      console.log("error request", error.response.status);
       return Promise.reject(error);
     }
   );
@@ -23,6 +24,11 @@ export const withResponseTimeMeasures = (axiosInstance) => {
       return response;
     },
     (error) => {
+      if (typeof error.response === "undefined") {
+        error.response = {};
+        error.response.status = 523;
+        error.response.data = "You've reached requests limits of Github API";
+      }
       return Promise.reject(error);
     }
   );
--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,8 @@
 import axios from "axios";
-import { withResponseTimeMeasures } from "./helpers/requests";
+import { addInterceptors } from "./helpers/requests";
 
 const requests = axios.create({
   baseURL: "https://api.github.com",
 });
 
-export default withResponseTimeMeasures(requests);
+export default addInterceptors(requests);
