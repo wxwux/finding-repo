2978cec5a6545becccb150b8b08bc83c8f34aff9
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-04-24 20:32:20 +0300

    history reducer test

--- src/store/reducers/__tests__/searchHistoryReducer.spec.js
+++ src/store/reducers/__tests__/searchHistoryReducer.spec.js
@@ -1,3 +1,10 @@
 import searchHistoryReducer from "../searchHistoryReducer";
+import { addSearchHistoryItem } from "../../actions";
 
+it("adds used item to the first position", () => {
+  const initialState = ["one", "two", "three"];
+  const action = addSearchHistoryItem("three");
+  const result = ["three", "one", "two"];
 
+  expect(searchHistoryReducer(initialState, action)).toStrictEqual(result);
+});
