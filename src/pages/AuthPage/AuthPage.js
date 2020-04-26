3796223b3018f14b7fbaff3bd62ff213726a1097
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-04-26 15:01:16 +0300

    added success message

--- src/pages/AuthPage/AuthPage.js
+++ src/pages/AuthPage/AuthPage.js
@@ -1,23 +1,27 @@
 import React, { useEffect } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useHistory } from "react-router-dom";
 import { getParamFromQueryString } from "../../helpers/queries";
 import { connect } from "react-redux";
 import { fetchUserRequest, fetchTokenRequest } from "../../store/actions";
 import useStyles from "./AuthPageUITheme";
-import AssignmentTurnedInIcon from "@material-ui/icons/AssignmentTurnedIn";
+import DoneIcon from "@material-ui/icons/Done";
+import Typography from "@material-ui/core/Typography";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import ErrorOutlineIcon from "@material-ui/icons/ErrorOutline";
 
 const AuthPage = ({ fetchUserRequest, fetchTokenRequest, token }) => {
   const location = useLocation();
   const classes = useStyles();
+  const history = useHistory();
 
   useEffect(() => {
     const code = getParamFromQueryString(location.search, "code");
     if (code) {
       fetchTokenRequest(code);
+    } else {
+      history.replace("/");
     }
-  }, [fetchTokenRequest, location.search]);
+  }, [fetchTokenRequest, history, location.search]);
 
   if (token.pending) {
     return <CircularProgress className={classes.icon} />;
@@ -27,7 +31,14 @@ const AuthPage = ({ fetchUserRequest, fetchTokenRequest, token }) => {
     return <ErrorOutlineIcon className={[classes.icon, classes.error]} />;
   }
 
-  return <AssignmentTurnedInIcon className={[classes.icon, classes.success]} />;
+  return (
+    <>
+      <DoneIcon className={[classes.icon, classes.success]} />
+      <Typography variant="h5" component="h5">
+        You're about to be redirecred back now
+      </Typography>
+    </>
+  );
 };
 
 const mapDispatchToPros = { fetchUserRequest, fetchTokenRequest };
--- src/pages/AuthPage/AuthPageUITheme.js
+++ src/pages/AuthPage/AuthPageUITheme.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   icon: {
-    fontSize: 100,
+    fontSize: 50,
     marginTop: theme.spacing(8)
   },
   success: {
