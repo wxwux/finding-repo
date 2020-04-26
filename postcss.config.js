991c938c7279a4f22a7cf407de1223bf61b83868
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-04-26 18:40:47 +0300

    added base config

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+module.exports = {
+  plugins: [
+    require("autoprefixer")({
+      cascade: false
+    })
+  ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = (env, argv) => {
+  process.env.BABEL_ENV = argv.mode;
+  const config = {
+    entry: "./src/index.js",
+    output: {
+      path: path.resolve(__dirname, "./dist"),
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+        },
+        {
+          test: /\.(js)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader", "postcss-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./public/index.html",
+      }),
+    ],
+    devServer: {
+      historyApiFallback: true,
+      noInfo: false,
+      overlay: true,
+    },
+    devtool: "#eval-source-map",
+  };
+
+  return config;
+};
