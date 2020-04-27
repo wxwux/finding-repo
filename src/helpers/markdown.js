1e7408edb11bab5166669bb5bc43e4c17196bb85
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-04-27 23:06:53 +0300

    cut off tags from markdown

--- src/helpers/markdown.js
+++ src/helpers/markdown.js
@@ -2,15 +2,15 @@ import { decodeToUnicode } from "./base64";
 import MarkdownIt from "markdown-it";
 
 export const cutOffTheBadges = (markdown) => {
-  const badgeRegex = /(\[!)(.*)(\]|\))/gm;
+  const badgeRegex =/((\[!)(.*)(\]|\)))|(<(.*)>)/gm;
+
   return markdown.replace(badgeRegex, "");
 };
 
 export const getHtmlFromMarkdownBase64 = (base64string) => {
   const content = decodeToUnicode(base64string);
-  const markdown = new MarkdownIt({
-    html: true,
-  });
+  const markdown = new MarkdownIt();
   const purifiedMarkdown = cutOffTheBadges(content);
+
   return markdown.render(purifiedMarkdown);
 };
